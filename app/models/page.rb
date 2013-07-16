cd2148dd5d3323642c27ec39bffb938aabb1f8d4
novoselov <novsem@gmail.com>
2013-07-16 11:36:56 +0300

    pages: add autofilling for slug and specs for it

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,11 +7,12 @@ class Page < ActiveRecord::Base
 
   acts_as_list
 
-  accepts_nested_attributes_for :seo, :allow_destroy => true, :reject_if => :all_blank
-  validates :slug, presence: true, uniqueness: true
-
   translates :content, :name
+
+  accepts_nested_attributes_for :seo, :allow_destroy => true, :reject_if => :all_blank
   accepts_nested_attributes_for :translations
+
+  validates :slug, presence: true, uniqueness: true
   validates :name, presence: true
 
   # Зачем это здесь?
@@ -22,12 +23,24 @@ class Page < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug
 
+  before_validation :fill_slug
+
   default_scope order('position')
   scope :visible, -> { where(hidden: false) }
   scope :invisible, -> { where(hidden: true) }
   scope :without, (lambda do |field, values|
-    raise "Unknown field :#{field} in Box model" unless field.to_s.in? attribute_names
+    raise "Unknown field :#{field} in Page model" unless field.to_s.in? attribute_names
     values = [values] unless values.is_a? Array
     where("#{field} NOT IN (?)", values)
   end)
+
+  private
+
+  def fill_slug
+    if slug.blank?
+      slug = translations.find{|t| t.locale == :en}.try(:name)
+      slug = slug || translations.find{|t| t.locale == :ru}.try(:name)
+      self.slug = Russian::translit(slug).parameterize if slug.present?
+    end
+  end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -11,19 +11,45 @@ describe Page do
   describe '#name' do
     it 'should not be empty' do
       page.name = ' '
-      page.should be_invalid
+      page.should have(1).errors_on(:name)
     end
   end
 
   describe '#slug' do
-    it 'should not be empty' do
-      page.slug = ' '
-      page.should be_invalid
-    end
-
     it 'should be unique' do
       FactoryGirl.create :page, :slug => page.slug
-      page.should be_invalid
+      page.should have(1).errors_on(:slug)
+    end
+
+    context 'when slug is blank' do
+      context 'when there is only russian translation' do
+        it 'should be filled with transliterated name' do
+          rus = FactoryGirl.build(:page_translation, locale: :ru, name: 'Всероссийская ассоциация ежей')
+          page.translations = [rus]
+          page.slug = ' '
+          page.should be_valid
+          page.slug.should == 'vserossiyskaya-assotsiatsiya-ezhey'
+        end
+      end
+
+      context 'when there is english translation' do
+        it 'should be filled with parameterized english name' do
+          eng = FactoryGirl.build(:page_translation, locale: :en, name: 'Russian hedgehogs assosiation')
+          rus = FactoryGirl.build(:page_translation, locale: :ru, name: 'Всероссийская ассоциация ежей')
+          page.translations = [eng, rus]
+          page.slug = ' '
+          page.should be_valid
+          page.slug.should == 'russian-hedgehogs-assosiation'
+        end
+      end
+
+      context 'when there are not translations' do
+        it 'should be invalid' do
+          page.translations = []
+          page.slug = ' '
+          page.should have(1).errors_on(:slug)
+        end
+      end
     end
   end
 
