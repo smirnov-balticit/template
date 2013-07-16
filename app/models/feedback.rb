c03501fec6401ba053d10c4cad0cd86d8fcba746
novoselov <novsem@gmail.com>
2013-07-16 12:40:29 +0300

    feedback: add client side validation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,5 +2,5 @@ class Feedback < ActiveRecord::Base
   attr_accessible :email, :message
 
   validates :email, presence: true, email: true
-  validates :message, presence: true, length: { minimum: 6, maximum: 2048 }
+  validates :message, presence: true, length: { minimum: 10, maximum: 2048 }
 end
