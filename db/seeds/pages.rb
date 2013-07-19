55f01455bbc315352b8a455008f5f55a62feb30f
Artyom Smirnov <smirnov@balticit.ru>
2013-07-19 10:03:05 +0300

    add position-field to seed for page model

--- db/seeds/pages.rb
+++ db/seeds/pages.rb
@@ -1,5 +1,6 @@
 #coding: utf-8
 
 Page.reset_column_information
-FactoryGirl.create(:page, slug: 'index')
-FactoryGirl.create(:page, slug: 'contacts')
+[:index, :contacts].each_with_index do |slug, index|
+  FactoryGirl.create(:page, slug: slug, position: index + 1)
+end
